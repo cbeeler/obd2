c058d4b9e065193c20dc6599fc8c50316c2d853c
Nick <ndillon3157@gmail.com>
2018-02-03 20:05:18 -0500

    added submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+#include <libraries/OBD/OBD.h>
+//mpu6050
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_FT6206.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+//COBDI2C obd;
+Adafruit_FT6206 ctp = Adafruit_FT6206();
+
 
 void setup() {
-    // put your setup code here, to run once:
+    mag.begin();
 }
 
 void loop() {
