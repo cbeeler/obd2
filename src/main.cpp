904c9d2322212b22e87292128fba1e875c4029b5
Nick <ndillon3157@gmail.com>
2018-03-14 20:42:19 -0400

    tried to rearrange submodule

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,26 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 #include <libraries/OBD/OBD.h>
-//mpu6050
+//#include <OBD.h>
+//mpu6050 https://github.com/jrowberg/i2cdevlib/tree/master/Arduino/MPU6050
+//https://playground.arduino.cc/Main/MPU-6050
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_FT6206.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
-//COBDI2C obd;
 Adafruit_FT6206 ctp = Adafruit_FT6206();
+
+const int TFT_CS = 10;
+const int TFT_DC = 9;
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
+//COBDI2C obd;
+
 
 
 void setup() {
